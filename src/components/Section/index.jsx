bc52b197f80a36f69681a25bb499933d188b9c41
Júlia Ohara <juliaohara.br@gmail.com>
2023-03-07 22:10:58 -0300

    feat: add global style and remove unused files

---
+++ src/components/Section/index.jsx
@@ -0,0 +1,13 @@
+import { Card } from './styles';
+
+function Section() {
+  return(
+    <>
+      <Card>
+        <p>Olá</p>
+      </Card>
+    </>
+  )
+}
+
+export default Section;
---
+++ src/components/Section/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  width: 200px;
+  height: 200px;
+`
