1d7bb80fd3053416b340963c4523bb163932e604
Júlia Ohara <juliaohara.br@gmail.com>
2023-03-14 22:21:38 -0300

    feat: add pokedex api

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,24 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import { FormSearch, Input, Button } from './styles';
 import { FaSearch } from "react-icons/fa";
-import { useState } from 'react';
 
 function Form() {
 
   const [valueInput, setValue] = useState('');
+  const apiPokemon = "https://pokeapi.co/api/v2/pokemon/";
 
-  const getValue = (e) => {
+  useEffect(() =>{
+    getPokemon();
+  })
+  
+  const getPokemon = () =>{
+    axios.get(apiPokemon)
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
+  }
+
+  const getValueInput = (e) => {
     e.preventDefault()
     setValue(e.target.value);
-
-
-    console.log(e.target.value)
   }
 
   return(
     <>
       <FormSearch>
-        <Input placeholder="Digite um número ou nome de um Pokemon" type="text" value={valueInput} onChange={getValue} />
-        
+        <Input placeholder="Digite um número ou nome de um Pokemon" type="text" value={valueInput} onChange={getValueInput} />
         <Button><FaSearch/></Button>
       </FormSearch>
       {valueInput}
