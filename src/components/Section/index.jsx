33d009614505b4380672d46dd9ce13328202f89b
Júlia Ohara <juliaohara.br@gmail.com>
2025-06-09 17:06:25 -0300

    feat/remove fragment tag from section component

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,15 +1,13 @@
-import { Container, Title } from './styles';
-import Form from '../Form';
+import { Container, Title } from "./styles";
+import Form from "../Form";
 
 function Section() {
-  return(
-    <>
-      <Container>
-        <Title>Pokédex</Title>
-        <Form />
-      </Container>
-    </>
-  )
+  return (
+    <Container>
+      <Title>Pokédex</Title>
+      <Form />
+    </Container>
+  );
 }
 
 export default Section;
